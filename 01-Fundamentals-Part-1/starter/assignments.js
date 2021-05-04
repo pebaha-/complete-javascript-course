16f3a8a92142ab83a40410a46f2926c9dd1e8bcb
Peter Bartram Hansen <pebaha@gmail.com>
2021-05-04 22:09:55 +0200

    Practice Assignment step

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,5 @@
+let country = "Denmark";
+let continent = "Europe";
+let population = 5800000;
+
+console.log(country + " is in " + continent + " and has a population of ~" + population + " as of 2021");

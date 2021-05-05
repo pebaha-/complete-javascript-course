839cbb01e7035c52389eb659728e6105e60868e8
Peter Bartram Hansen <pebaha@gmail.com>
2021-05-05 19:59:41 +0200

    Data Types (end of lesson)

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,31 @@
+/*
 let js = "JavaScript is amazing!";
 console.log(63 - 19 - 2);
 
 let firstName = "Peter";
 console.log(firstName);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Peter');
+
+javascriptIsFun = 'YES!';
+
+console.log(typeof javascriptIsFun);
+
+let year;
+
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+
+console.log(year);
+console.log(typeof year);
+
+console.log(typeof null);

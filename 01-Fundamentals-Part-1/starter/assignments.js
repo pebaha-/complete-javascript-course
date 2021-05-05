b6a4decd6eef3f4ee4539f598e161c4ca6ab8783
Peter Bartram Hansen <pebaha@gmail.com>
2021-05-05 21:37:35 +0200

    Practice Assignment step

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -1,12 +1,12 @@
-let country = "Denmark";
-let continent = "Europe";
+const country = "Denmark";
+const continent = "Europe";
 let population = 5800000;
 
 console.log(country + " is in " + continent + " and has a population of ~" + population + " as of 2021");
 
 ////////////////////
 
-let isIsland = false;
-let language;
+const isIsland = false;
+const language = "Danish";
 
 console.log(country + " has a population of ~" + population + " as of 2021. Is it an island? " + isIsland + ". The language is " + language + ".");

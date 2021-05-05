216312c92e9a88db86afb17e11aa50892f0b7d0a
Peter Bartram Hansen <pebaha@gmail.com>
2021-05-05 20:03:03 +0200

    Practice Assignment step

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -3,3 +3,10 @@ let continent = "Europe";
 let population = 5800000;
 
 console.log(country + " is in " + continent + " and has a population of ~" + population + " as of 2021");
+
+////////////////////
+
+let isIsland = false;
+let language;
+
+console.log(country + " has a population of ~" + population + " as of 2021. Is it an island? " + isIsland + ". The language is " + language + ".");

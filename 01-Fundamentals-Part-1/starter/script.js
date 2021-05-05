3b952dd0355e26d589cd2d259305a95ff6e51843
Peter Bartram Hansen <pebaha@gmail.com>
2021-05-05 22:44:33 +0200

    Basic Operators (end of lesson)

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -29,8 +29,6 @@ console.log(typeof year);
 
 console.log(typeof null);
 
-*/
-
 let age = 30;
 age = 31;
 
@@ -44,3 +42,34 @@ job = 'Teacher';
 
 lastName = 'Smith';
 console.log(lastName);
+
+*/
+
+// Math operators
+const nowYear = 2037;
+const ageJonas = nowYear - 1991;
+const ageSarah = nowYear - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 (2^3 or 2 * 2 * 2 = 8)
+
+const firstName = 'John';
+const lastName = 'Smith';
+console.log(firstName + ' ' + lastName);
+
+// Assignment operators
+let x = 10 + 5;
+x += 10; // x = x + 10 equals 25
+x *= 4; // x = x * 4 equals 100
+x++; // x = x + 1
+x--; // x = x - 1
+x--;
+console.log(x);
+
+// Comparison operators
+console.log(ageJonas > ageSarah); // <, >, <=, >=
+const isFullAge = ageSarah >= 18;
+console.log(isFullAge);
+
+console.log(nowYear - 1991 > nowYear - 2018);

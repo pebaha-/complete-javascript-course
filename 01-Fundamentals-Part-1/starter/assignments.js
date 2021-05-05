5289d1dfd3e191735b77e77b08c6889666dbadc0
Peter Bartram Hansen <pebaha@gmail.com>
2021-05-05 22:50:29 +0200

    Practice Assignment step

--- 01-Fundamentals-Part-1/starter/assignments.js
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -10,3 +10,17 @@ const isIsland = false;
 const language = "Danish";
 
 console.log(country + " has a population of ~" + population + " as of 2021. Is it an island? " + isIsland + ". The language is " + language + ".");
+
+console.log("Half of the population of " + country + " is " + population / 2);
+
+population++;
+console.log(population);
+
+const populationFinland = 5500000;
+console.log(country + " has a larger population than Finland? " + (populationFinland < population));
+
+const averagePopulation = 33000000;
+console.log(country + " has a smaller population than the average country? " + (averagePopulation > population));
+
+const description = country + " is in " + continent + " and its " + population + " people speak " + language;
+console.log(description);

de7068d28e9f1138b851573a3b923e1c6315840c
Peter Bartram Hansen <pebaha@gmail.com>
2021-05-06 10:14:32 +0200

    Operator Precedence (end of lesson)

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -43,8 +43,6 @@ job = 'Teacher';
 lastName = 'Smith';
 console.log(lastName);
 
-*/
-
 // Math operators
 const nowYear = 2037;
 const ageJonas = nowYear - 1991;
@@ -73,3 +71,18 @@ const isFullAge = ageSarah >= 18;
 console.log(isFullAge);
 
 console.log(nowYear - 1991 > nowYear - 2018);
+
+*/
+
+const nowYear = 2037;
+const ageJonas = nowYear - 1991;
+const ageSarah = nowYear - 2018;
+
+console.log(nowYear - 1991 > nowYear - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5; // x = y = 10
+console.log(x, y)
+
+const averaAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averaAge);
